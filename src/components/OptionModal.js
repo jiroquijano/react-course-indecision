b714fc3d4484a20008428491ad1a1e2e9bf91b87
jiroquijano <jiroquijano@gmail.com>
2020-05-25 15:54:19 +0800

    Added additional improvements to styling using @media and viewport meta

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,12 @@ const OptionModal = (props) =>(
         isOpen = {!!props.selectedOption}
         contentLabel = "Selected Option"
         onRequestClose={props.unselect}
+        closeTimeoutMS={200}
+        className="modal"
     >
-        <h3>Selected Option</h3>
-        <h4>{props.selectedOption}</h4>
-        <button onClick={props.unselect}>OK</button>
+        <h3 className='modal__title'>Selected Option</h3>
+        <h4 className='modal__body'>{props.selectedOption}</h4>
+        <button className='button' onClick={props.unselect}>OK</button>
     </Modal>
 );
 
